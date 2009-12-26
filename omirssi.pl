2c67fb821e4f238c8cc13b18ace28172f77d7b61
ay1244 <ay1244@53b86e26-f192-11de-b2c3-d77de7315633>
2009-12-26 10:01:17 +0000

    Fixed renaming Added comments for documentation Added settings and such

--- omirssi.pl
+++ omirssi.pl
@@ -1,3 +1,14 @@
+# omirssi.pl
+#
+# How to use
+# ----------
+#
+# /om_connect - start a chat with a stranger
+# /om_disconnect - stop a chat
+# /om_say - send a message
+# /om_rename - rename the user
+#
+
 use warnings;
 use strict;
 
@@ -6,25 +17,37 @@ use WWW::Omegle;
 
 use vars qw($VERSION %IRSSI);
 
-$VERSION = '0.10';
+$VERSION = '0.11';
 %IRSSI = (
     authors =>  "William Orr",
     contact =>  "will\@worrbase.com",
     name    =>  "Omirssi",
-    description => "Omegle!",
+    description => "Omegle on irssi!",
     license =>  "MIT License",
 );
 
+# Let's add some configurability...
+Irssi::settings_add_str('omirssi', 'stranger_name', 'Stranger');
+Irssi::settings_add_str('omirssi', 'your_name', 'You');
+Irssi::settings_add_str('omirssi', 'omirssi_window_prefix', 'omegle');
+Irssi::settings_add_str('omirssi', 'connect_message', '');
+Irssi::settings_add_str('omirssi', 'disconnect_message', '');
+Irssi::settings_add_bool('omirssi', 'warn_messages', 0);
+
 # The Omegle perl module is so incomplete, that this is necessary
-$SIG{__WARN__} = sub {};
+if (not Irssi::settings_get_bool('warn_messages')) {
+    $SIG{__WARN__} = sub {};
+}
 
 my $window;
 my $om = WWW::Omegle->new(on_chat => \&stranger_chat,
                           on_disconnect => sub { 
-                              $window->print("Stranger disconnected"); });
-my $stranger = "Stranger";
+                              $window->print("Stranger disconnected"); 
+                          });
+my $stranger = Irssi::settings_get_str('stranger_name');
 my $timeout;
-
+my $window_prefix = Irssi::settings_get_str('omirssi_window_prefix');
+my $you = Irssi::settings_get_str('your_name');
 
 sub om_poke {
     $om->poke;
@@ -59,32 +82,33 @@ sub om_say {
     my ($message) = @_;
 
     $om->say("$message");
-    $window->print("You: $message", MSGLEVEL_CLIENTCRAP);
+    $window->print("$you: $message", MSGLEVEL_CLIENTCRAP);
 }
 
 sub om_rename {
-    unless (@_ == 1) {
-        $window->print("/om_rename name");
+    unless (@_ == 3) {
+        $window->print("/om_rename name", MSGLEVEL_CLIENTCRAP);
         return;
     }
 
     $stranger = shift;
 }
 
-$window = Irssi::window_find_name('omegle');
+$window = Irssi::window_find_name($window_prefix);
 
 if (!$window) {
-    Irssi::active_win()->print("Couldn't find a window called omegle, creating...");
-    $window = Irssi::Windowitem::window_create('omegle', 1);
-    $window->set_name('omegle');
+    Irssi::active_win()->print("Couldn't find a window called $window_prefix, creating...");
+    $window = Irssi::Windowitem::window_create($window_prefix, 1);
+    $window->set_name($window_prefix);
 }
 
 if ($window) {
+    # Command bindings
     Irssi::command_bind(om_connect => \&om_connect);
     Irssi::command_bind(om_disconnect => \&om_disconnect);
     Irssi::command_bind(om_say => \&om_say);
     Irssi::command_bind(om_rename => \&om_rename);
 } else {
     Irssi::active_win()->print("Couldn't create window, create a window yourself " .
-                               "and call it omegle.");
+                               "and call it $window_prefix.");
 }
